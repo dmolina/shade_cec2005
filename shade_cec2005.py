298c8a96d8351a36a1ab0b7014db6ed6dc3d030c
Daniel Molina <daniel.molina@uca.es>
2015-10-31 15:10:43 +0100

    Fix error with replace population.

--- shade_cec2005.py
+++ shade_cec2005.py
@@ -54,7 +54,7 @@ def main(args):
 
     for r in range(params.run):
         result,bestIndex = shade.improve(fitness_fun, info, dim, 10000*dim,
-                                         name_output=output, replace=True, times=params.run, popsize=100, ignoreLimits=ignoreLimits)
+                                         name_output=output, replace=False, times=params.run, popsize=100, ignoreLimits=ignoreLimits)
         best_sol = result.solution
         best_fitness = result.fitness
 
